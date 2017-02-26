211b589d5b51013812ce49d7f6c2535da8ef2845
Addy Alago <aalag001@odu.edu>
2017-02-26 13:01:17 -0500

    Home, layout, registration pages added to templates.
    Registration page functional and adds new user to database but does not validate any information (ie duplicate entries allowed, password not masked or checked for complexity, email address is just a string).
    
    Rough CSS sheet added to project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
@@ -12,10 +12,26 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity) # /auth
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/register/')
+def register():
+
+    return render_template('register.html')
 
 
 
 
-api.add_resource(UserRegister, '/register') # register endpoint
+
+
+api.add_resource(UserRegister, '/registration') # register endpoint
 
 app.run(port=5000, debug=True)
