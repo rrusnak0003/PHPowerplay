605f8b6108fe4d0bfbd563bde4f336a0f0c8ebe0
Addy Alago <aalag001@odu.edu>
2017-02-25 19:43:40 -0500

    Create temporary SQLite DB for code testing purposes.

--- .idea/create_tables.py
+++ .idea/create_tables.py
@@ -2,6 +2,8 @@ import sqlite3
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
+
+#create temp sqlite3 database to hold users. Will be replaced with main projects sql instance.
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY , username text, password text)"
 cursor.execute(create_table)
 
