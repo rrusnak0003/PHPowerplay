ffa06b641fbbe080273511c28d267f6da10edfb2
Addy Alago <aalag001@odu.edu>
2017-02-25 19:48:11 -0500

    Addded Register endpoint for user registration.

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+from user import UserRegister
+
+#app.py creates application for json endpoints for use with front end.
+
+app = Flask(__name__)
+app.secret_key = '&6yhgTrFjKilo)9KJhjGHg'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
+
+
+
+
+
+api.add_resource(UserRegister, '/register') # register endpoint
+
+app.run(port=5000, debug=True)
