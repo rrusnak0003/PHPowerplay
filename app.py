bc107edf877478d68cb96fd205154973021bd54f
Addy Alago <aalag001@odu.edu>
2017-03-05 23:29:22 -0500

    Added login, logout, register  and home templates. Login is now handled by session data instead of JWT. Templates styled using bootstrap

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 
 from flask import Flask, request, render_template
+from flask import session
 from flask_restful import Api
 from flask_jwt import JWT
 
+from models.user import UserModel
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.powerplant import Powerplant, PowerplantList
 from resources.region import Region, RegionList
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -17,13 +18,15 @@ app.secret_key = 'addy'
 api = Api(app)
 
 
-#app.py creates application for json endpoints for use with front end.
+# app.py creates application for json endpoints for use with front end.
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-jwt = JWT(app, authenticate, identity) # /auth
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
 
 @app.route('/')
 def home():
@@ -37,23 +40,61 @@ def about():
 
 @app.route('/register/')
 def register():
-
-    return render_template('register.html')
-
-
-api.add_resource(Region, '/region/<string:name>')
-api.add_resource(Powerplant, '/powerplant/<string:name>')
-api.add_resource(PowerplantList, '/powerplants')
-
-api.add_resource(RegionList, '/regions')
-#api.add_resource(UserRegister, '/register')
+    if session['username'] == None:
+        return render_template('register.html')
+    return render_template('home.html')
 
 
+@app.route('/login')
+def login():
+    if session['username'] == None:
+        return render_template('login.html')  # www.mysite.com/api/login
+    return render_template('home.html')
 
-api.add_resource(UserRegister, '/registration') # register endpoint
+@app.route('/logout')
+def logout():
+    session['username'] = None
+    return render_template('home.html')  # www.mysite.com/api/login
 
+
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    if session['username'] == None:
+        email = request.form['email']
+        password = request.form['password']
+        username = request.form['username']
+
+        UserModel.register(username, password, email)
+
+        return render_template('profile.html', username=session['username'])
+    return render_template('home.html')
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():
+    username = request.form['username']
+    password = request.form['password']
+
+    if UserModel.login_valid(username, password):
+        UserModel.login(username)
+    else:
+        session['username'] = None
+
+    return render_template('profile.html', username=session['username'])
+
+
+# api.add_resource(Region, '/region/<string:name>')
+# api.add_resource(Powerplant, '/powerplant/<string:name>')
+# api.add_resource(PowerplantList, '/powerplants')
+# api.add_resource(RegionList, '/regions')
+# api.add_resource(UserRegister, '/register')
+
+
+
+# api.add_resource(UserRegister, '/auth/register')  # register endpoint
 
 if __name__ == '__main__':
     from db import db
+
     db.init_app(app)
     app.run(port=5000, debug=True)
--- resources/user.py
+++ resources/user.py
@@ -2,10 +2,9 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
+
 class UserRegister(Resource):
-
     parser = reqparse.RequestParser()
-
 
     parser.add_argument('username',
                         type=str,
@@ -17,16 +16,19 @@ class UserRegister(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
+    parser.add_argument('email',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
 
     def post(self):
-
-
         data = UserRegister.parser.parse_args()
-
-
 
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
+        if UserModel.find_by_email(data['email']):
+            return {"message": "A user with that email already exists"}, 400
 
         user = UserModel(**data)
         user.save_to_db()
