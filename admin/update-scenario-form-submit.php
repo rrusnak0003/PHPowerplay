bb2f96cc33282e15f735c8d4b73a7bf379d14605
Addy Alago <aalag001@odu.edu>
2017-04-12 19:14:43 -0400

    changed form styling and fixed form functionality

--- admin/update-scenario-form-submit.php
+++ admin/update-scenario-form-submit.php
@@ -17,7 +17,7 @@ $nomcap5yr = $_GET['nominal_capacity5yr'];
 $mincap5yr = $_GET['min_capacity5yr'];
 $maxcap5yr = $_GET['max_capacity5yr'];
 $coemissions5yr = $_GET['co_emissions5yr'];
-
+echo("fixcost" + $fixcost);
 
 
 
@@ -29,10 +29,7 @@ function findTech($db, $year){
 
     $tech = $db->query($query);
 
-    /*
-    ** users is a mysqli_result object
-    ** so if num_rows of the object is 0 there isn't a match
-    */
+
     if($tech->num_rows == 0){
         return true;
     }
@@ -44,24 +41,55 @@ function findTech($db, $year){
 
 
 
-function editScenario($db){
 
 
 
-    if(findTech($db, 2016)){
-        $query = "INSERT INTO technology_cost (year, technology, technical, overnight_capital, fixed_cost, variable_cost, min_capacity, nominal_capacity, carbon_dioxide)
-                  VALUES (2016, 'Onshore Wind', 'Onshore Wind (WN)', 0, $fixcost,$varcost, $mincap, $nomcap, $coemissions )";
-        $insert = $db->query($query);
-    }
-    else {
-        $query = "UPDATE technology_cost 
-                SET year = 2016, technology = 'Onshore_Wind', technical = 'Onshore Wind (WN)', overnight_capital = 0, fixed_cost = $fixcost, variable_cost = $varcost, min_capacity = $mincap, nominal_capacity =  $nomcap, carbon_dioxide= $coemissions
+
+
+
+
+
+
+require('../login.php');
+
+$db = new mysqli($hn, $un, $pw, $db);
+
+if($db->connect_error)
+{
+
+    die($db->connect_error);
+
+}
+if(findTech($db, 2016)){
+    $query = "INSERT INTO technology_cost (year, technology, technical, overnight_capital, fixed_cost, variable_cost, min_capacity, nominal_capacity, max_capacity,  carbon_dioxide)
+                  VALUES (2016, 'Onshore Wind', 'Onshore Wind (WN)', $capcost, $fixcost,$varcost, $mincap, $nomcap,$maxcap, $coemissions )";
+    $insert = $db->query($query);
+
+}
+else {
+    $query = "UPDATE technology_cost 
+                SET year = 2016, technology = 'Onshore Wind', technical = 'Onshore Wind (WN)', overnight_capital = $capcost, fixed_cost = $fixcost, variable_cost = $varcost, min_capacity = $mincap, max_capacity = $maxcap, nominal_capacity =  $nomcap, carbon_dioxide= $coemissions
                 WHERE year = 2016 AND  technology='Onshore Wind'";
-        $insert = $db->query($query);
-    }
-
+    $insert = $db->query($query);
 
 }
 
 
+if(findTech($db, 2021)){
+    $query = "INSERT INTO technology_cost (year, technology, technical, overnight_capital, fixed_cost, variable_cost, min_capacity, nominal_capacity, max_capacity,  carbon_dioxide)
+                  VALUES (2021, 'Onshore Wind', 'Onshore Wind (WN)', $capcost5yr, $fixcost5yr,$varcost5yr, $mincap5yr, $nomcap5yr,$maxcap5yr, $coemissions5yr )";
+    $insert = $db->query($query);
+
+}
+else {
+    $query = "UPDATE technology_cost 
+                SET year = 2021, technology = 'Onshore Wind', technical = 'Onshore Wind (WN)', overnight_capital = $capcost5yr, fixed_cost = $fixcost5yr, variable_cost = $varcost5yr, min_capacity = $mincap5yr, max_capacity = $maxcap5yr, nominal_capacity =  $nomcap5yr, carbon_dioxide= $coemissions5yr
+                WHERE year = 2021 AND  technology='Onshore Wind'";
+    $insert = $db->query($query);
+
+}
+
+
+
+header('Location: ./update-scenario-form.php');
 ?>
